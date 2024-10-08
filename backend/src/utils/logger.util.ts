6b2c824048bb0380bed49d31491728f3044da5a8
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-10-08 16:07:16 +0200

    getting ready to switch to next JS 14 setup

--- backend/src/utils/logger.util.ts
+++
@@ -1,6 +0,0 @@
-import devLogger from './logger-dev.util';
-import prodLogger from './logger-prod.util';
-
-const logger = process.env.NODE_ENV === 'production' ? prodLogger : devLogger;
-
-export default logger;
