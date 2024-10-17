ebbc44fb1edc9824c9c4174dd48a5873d114a1d2
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-10-17 15:43:39 +0200

    added global prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,6 +2,10 @@
 
 import { PrismaClient } from '@prisma/client';
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
